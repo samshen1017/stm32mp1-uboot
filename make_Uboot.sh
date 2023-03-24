7cc97451702564de9e21125d53eb53ca79abcdc2
墨鸿苍穹 <samshen1017@sina.com>
2023-03-24 20:11:30 +0800

    uboot编译，部分板子需要TF-A和Optee的FIP

--- make_Uboot.sh
+++ make_Uboot.sh
@@ -3,7 +3,7 @@
 UBOOT_DIR=$PWD/u-boot-stm32mp-v2021.10-stm32mp-r2
 ENV_SET=/home/isaac/toolchain/st/stm32mp1/4.0.4-openstlinux-5.15-yocto-kirkstone-mp1-v22.11.23/environment-setup-cortexa7t2hf-neon-vfpv4-ostl-linux-gnueabi
 
-cd $TF_A_DIR
+cd $UBOOT_DIR
 
 #设置编译器
 source $ENV_SET
