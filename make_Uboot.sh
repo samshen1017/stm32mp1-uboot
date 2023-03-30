50893bc0e425ab8f6cbf917dca3878d8b36cbb1f
墨鸿苍穹 <samshen1017@sina.com>
2023-03-30 21:29:22 +0800

    创建openedv设备树

--- make_Uboot.sh
+++ make_Uboot.sh
@@ -3,15 +3,19 @@
 UBOOT_DIR=$PWD/u-boot-stm32mp-v2021.10-stm32mp-r2
 ENV_SET=/home/isaac/toolchain/st/stm32mp1/4.0.4-openstlinux-5.15-yocto-kirkstone-mp1-v22.11.23/environment-setup-cortexa7t2hf-neon-vfpv4-ostl-linux-gnueabi
 
+# 更换需要编译的目标板
+# TARGET_NAME=stm32mp157c-astro
+TARGET_NAME=stm32mp157d-openedv
+
 cd $UBOOT_DIR
 
 #设置编译器
 source $ENV_SET
 
-if [ $1 == "astro" ]; then
+if [ $1 == "debug" ]; then
     unset -v CFLAGS LDFLAGS
-    make ARCH=arm CROSS_COMPILE=arm-ostl-linux-gnueabi- O="$PWD/../build" stm32mp157c_astro_defconfig
-    make ARCH=arm CROSS_COMPILE=arm-ostl-linux-gnueabi- O="$PWD/../build" DEVICE_TREE=stm32mp157c-astro all
+    make ARCH=arm CROSS_COMPILE=arm-ostl-linux-gnueabi- O="$PWD/../build" "$TARGET_NAME"_defconfig
+    make ARCH=arm CROSS_COMPILE=arm-ostl-linux-gnueabi- O="$PWD/../build" DEVICE_TREE=$TARGET_NAME all
 
 elif [ $1 == "copy" ]; then
     mkdir -p ../../image
